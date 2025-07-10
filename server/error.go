08558512700813a374e4f3b80072dd6972a0ee93
AhmedTalbii <ahmedtalbi459@gmail.com>
2025-07-10 05:50:25 +0100

    undestanding hard procces + add comments

--- server/error.go
+++ server/error.go
@@ -20,6 +20,11 @@ func CssHandle(w http.ResponseWriter, r *http.Request, mux *http.ServeMux) {
 		renderError(w, r)
 		return
 	}
+	// this concept is somme how hard 
+	// first dir returns the file systeme 
+	// secend http.file server creates a fileHandler — an object that knows how to serve files It doesn’t run yet
+	// third strip prefix delet "/templates/css/" and leave the style.css and return it to the inner file server again
+	// Fourth the last thing is write response using .ServeHTTP(w, r) and also  using the cleaned path
 	http.StripPrefix("/templates/css/", http.FileServer(http.Dir("templates/css"))).ServeHTTP(w, r)
 }
 
